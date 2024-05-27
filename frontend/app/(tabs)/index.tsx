edf28ac9688cd8eb84abcfe4e6b7840c9f2bc5c4
leyew <102467346+itsme-zeix@users.noreply.github.com>
2024-05-27 18:35:50 +0800

    Added front page template

--- frontend/app/(tabs)/index.tsx
+++ frontend/app/(tabs)/index.tsx
@@ -1,70 +1,122 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import React from 'react';
+import { View, StyleSheet, SafeAreaView, ScrollView, Text, StatusBar } from 'react-native';
+import BusStopSearchBar from "@/components/BusStopSearchBar" 
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+interface BusStop {
+  busStopName: string;
+  distanceAway: string;
+  savedBuses: string[][];
+}
+
+const ClementiMRT: BusStop = {
+  busStopName: "Clementi MRT",
+  distanceAway: "~20m away",
+  savedBuses: [["96", "1", "12"], ["96A", "2", "6"]]
+}
+
+const KRMRT: BusStop = {
+  busStopName: "Kent Ridge MRT",
+  distanceAway: "~1500m away",
+  savedBuses: [["D1", "2", "7"], ["A1", "3", "9"], ["K", "1", "12"]]
+}
+
+const busStops: BusStop[] = [ClementiMRT, KRMRT];
+
+export const busCard = (bus: string[]) => {
+  return (
+    <View style={ styles.busCard }>
+      <Text style={{ fontSize: 24 }}> { bus[0] } </Text>
+      <Text style={{ fontSize: 10 }}> Arrival Time </Text> 
+      <View style={ styles.busTimings }> 
+        <View style={ styles.busTiming }> 
+          <Text style={{ color: "#30B800", fontSize: 22 }}>{ bus[1] }</Text>
+          <Text style={{ color: "#30B800", fontSize: 12 }}>min</Text>
+        </View>
+        <View style={ styles.busTiming }>
+          <Text style={{ color: "#DE8500", fontSize: 22 }}>{ bus[2] }</Text>
+          <Text style={{ color: "#DE8500", fontSize: 12 }}>min</Text>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export const busStopCard = (busStop: BusStop)  => {
+  return (
+    <View style={ styles.busStopCard }>
+      <View>
+        <Text style={{ fontSize: 20 }}> { busStop.busStopName } </Text>
+        <Text style={{ color: "626262", fontSize: 10 }}> { busStop.distanceAway } </Text>
+      </View>
+      <View style={ styles.busTimings }>
+        <Text>
+        {busStop.savedBuses.map((bus: string[], index: number) => (
+          <View key={index}>
+            {busCard(bus)}
+          </View>
+        ))}
+        </Text>
+      </View>
+    </View>
+  );
+}
 
 export default function HomeScreen() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Hello World!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+      <SafeAreaView style={styles.safeAreaViewContainer}>
+        <BusStopSearchBar></BusStopSearchBar>
+        <ScrollView style={styles.scrollView}>
+          {busStops.map((busStop, index) => (
+            <View key={index}>
+                {busStopCard(busStop)}
+            </View>
+          ))}
+        </ScrollView>
+      </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
+  safeAreaViewContainer: {
+    flex: 1,
+    flexDirection: 'column',
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  scrollView: {
+    flex: 1,
+    paddingLeft: 16,
+    paddingRight: 16,
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  busCard: {
+    height: 80,
+    width: 100,
+    borderColor: "#ccc",
+    borderWidth: 1,
+    padding: 6,
+    marginTop: 8,
+    marginHorizontal: 8,
+    justifyContent: "flex-start"
   },
+  busTimings: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  busTiming: {
+    flexDirection: "row", 
+    alignItems: "flex-end",
+    justifyContent: "space-between",
+
+  },
+  busStopCard: {
+    borderColor: "#ccc",
+    borderWidth: 1,
+    marginVertical: 8,
+    borderRadius: 5,
+    backgroundColor: "#fff",
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.2,
+    paddingVertical: 14,
+    paddingHorizontal: 16,
+  },
+
 });
