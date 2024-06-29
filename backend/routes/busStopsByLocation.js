5a09b8103467f94865836a4b5d323eb9b0f28107
leyew <102467346+itsme-zeix@users.noreply.github.com>
2024-06-29 18:34:34 +0900

    Feat[busStopsByLocation] Slight optimization in parallelism and refactor

--- backend/routes/busStopsByLocation.js
+++ backend/routes/busStopsByLocation.js
@@ -3,6 +3,12 @@ const express = require("express");
 const router = express.Router();
 const { Client } = require("pg");
 
+function logTime(label, startTime) {
+  const endTime = process.hrtime(startTime);
+  const timeTaken = (endTime[0] * 1e9 + endTime[1]) / 1e6; // convert to milliseconds
+  console.log(`${label}: ${timeTaken.toFixed(3)}ms`);
+}
+
 // Create bus stop objects (based on interface defined in front end, see comment inside the method) using bus stop info retrieved from database in getNearestBusStops method.
 async function generateBusStopsObject(stop) {
   // Interface in our tsx frontend for reference so we can reuse them when returning JSON.
@@ -17,7 +23,7 @@ async function generateBusStopsObject(stop) {
   //   savedBuses: BusService[];
   // }
 
-  // initialize bus stop objeect
+  // initialize bus stop object
   const busStopObject = {
     busStopName: stop.name,
     busStopId: String(stop.id),
@@ -87,11 +93,9 @@ async function getNearestBusStops(userLat, userLon) {
       .slice(0, 10); // Get the 10 nearest stops
 
     // Format the bus stops properly so that timings can be inserted easily once retrieved.
-    const arrBusStops = [];
-    for (stop of nearbyStops) {
-      const busStopObject = await generateBusStopsObject(stop);
-      arrBusStops.push(busStopObject);
-    }
+    const arrBusStops = await Promise.all(
+      nearbyStops.map((stop) => generateBusStopsObject(stop))
+    );
     return arrBusStops;
   } catch (err) {
     console.error(
@@ -107,123 +111,25 @@ async function getNearestBusStops(userLat, userLon) {
 // Functionally the same as calling /busStopArrivalTimes, but we avoid calling the internal API, despite the abstraction, as it will mean that we have to wait
 // for multiple calls to complete synchronously, increasing our API's response time.
 async function getArrivalTime(busStopsArray) {
-  for (const busStop of busStopsArray) {
-    if (busStop.busStopName.startsWith("NUSSTOP")) {
-      await (async (busStop) => {
-        const stopName = busStop.busStopName.substring(8); // substring(8) skips the first 8 characters 'NUSSTOP_'
-        try {
-          const username = process.env.NUSNEXTBUS_USER;
-          const password = process.env.NUSNEXTBUS_PASSWORD;
-
-          // Encode the credentials
-          const credentials = `${username}:${password}`;
-          const encodedCredentials = btoa(credentials);
-
-          const response = await fetch(
-            `https://nnextbus.nus.edu.sg/ShuttleService?busstopname=${stopName}`,
-            {
-              method: "GET",
-              headers: {
-                Authorization: `Basic ${encodedCredentials}`,
-              },
-            }
-          );
-
-          // Check if the response is ok and has a body
-          if (!response.ok) {
-            throw new Error(
-              `HTTP error from NUSNextBus API! status: ${response.status}`
-            );
-          }
-
-          const text = await response.text();
-          if (!text) {
-            throw new Error("Empty response body from NUSNextBus API");
-          }
-
-          const NUSReply = JSON.parse(text);
-          // We will process the NUSReply in a 2 step process:
-          // 1) reformat reply such that we can search the buses by name in a dict.
-          // 2) iterate through buses in our bus stop objects and retrieve the timings based on the name.
-
-          // REFORMAT
-          let shuttles = {};
-          for (let shuttle of NUSReply.ShuttleServiceResult.shuttles) {
-            shuttles[shuttle.name] = shuttle;
-          }
-          // ITERATE AND UPDATE BUS ARRIVAL TIMING
-          for (let busObject of busStop.savedBuses) {
-            const serviceName = busObject.busNumber;
-            if (shuttles[serviceName]) {
-              const shuttle = shuttles[serviceName];
-              if (shuttle._etas) {
-                // These are NUS buses. Public buses do not have ._etas field in NUSNextBus API response.
-                // Handle the cases of differing sizes of etas returned due to 0/1/2 next buses.
-                // ETA is not given in ISO time, so we have to calculate the ISO time based on mins till arrival.
-                const etaLength = shuttle._etas.length;
-                const currentTime = new Date();
-                if (etaLength == 0) {
-                  busObject.timings = ["N.A.", "N.A."];
-                } else if (etaLength == 1) {
-                  const arrivalTime = shuttle._etas[0].eta_s;
-                  const firstArrivalTime = new Date(
-                    currentTime.getTime() + arrivalTime * 1000
-                  ).toISOString();
-                  busObject.timings = [firstArrivalTime, "N.A."];
-                } else {
-                  const arrivalTime = shuttle._etas[0].eta_s;
-                  const nextArrivalTime = shuttle._etas[1].eta_s;
-                  const firstArrivalTime = new Date(
-                    currentTime.getTime() + arrivalTime * 1000
-                  ).toISOString();
-                  const secondArrivalTime = new Date(
-                    currentTime.getTime() + nextArrivalTime * 1000
-                  ).toISOString();
-                  busObject.timings = [firstArrivalTime, secondArrivalTime];
-                  busStop.busNumber = shuttle.caption; // Update NUS Bus Stop name to be the full name rather than the code name (i.e. YIH-OPP -> Opp Yusof Ishak House).
-                }
-              } else {
-                // Public bus timings obtained by NUSNextBus API is given in mins to arrival rather than ISO time.
-                // The code below converts arrival time in minutes to ISO time to maintain a standard response format.
-                const currentTime = new Date();
-                let arrivalTime = shuttles[serviceName].arrivalTime;
-                let nextArrivalTime = shuttles[serviceName].nextArrivalTime;
-
-                // Check if arrivalTime and nextArrivalTime are numbers (they can be Arr), if not, set them to 0
-                arrivalTime = isNaN(arrivalTime) ? 0 : arrivalTime;
-                nextArrivalTime = isNaN(nextArrivalTime) ? 0 : nextArrivalTime;
-
-                const firstArrivalTime = new Date(
-                  currentTime.getTime() + arrivalTime * 60000
-                ).toISOString();
-                const secondArrivalTime = new Date(
-                  currentTime.getTime() + nextArrivalTime * 60000
-                ).toISOString();
-
-                busObject.timings = [firstArrivalTime, secondArrivalTime];
-              }
-            }
-          }
-          // Update NUS Bus Stop name to be the full name rather than the code name (i.e. NUSSTOP_YIH-OPP -> NUSSTOP_Opp Yusof Ishak House).
-          busStop.busStopName =
-            "NUSSTOP_" + NUSReply.ShuttleServiceResult.caption;
-        } catch (error) {
-          console.error("Error fetching data from NUSNextBus API:", error);
-        }
-      })(busStop);
-    } else {
-      for (const bus of busStop.savedBuses) {
-        // logic is somewhat convoluted here, can be simplified by removing serviceNo to reduce the number of API calls.
-        // though that will require some gymnastics with inserting the timings (need to match serviceNo etc).
-        // Also, if we simply call the API by bus stop id, certain bus services will be missing because the api only responses for certain bus stops.
-        await (async (stopId, serviceNo) => {
+  await Promise.all(
+    busStopsArray.map(async (busStop) => {
+      if (busStop.busStopName.startsWith("NUSSTOP")) {
+        await (async (busStop) => {
+          const stopName = busStop.busStopName.substring(8); // substring(8) skips the first 8 characters 'NUSSTOP_'
           try {
+            const username = process.env.NUSNEXTBUS_USER;
+            const password = process.env.NUSNEXTBUS_PASSWORD;
+
+            // Encode the credentials
+            const credentials = `${username}:${password}`;
+            const encodedCredentials = btoa(credentials);
+
             const response = await fetch(
-              `http://datamall2.mytransport.sg/ltaodataservice/BusArrivalv2?BusStopCode=${stopId}&ServiceNo=${serviceNo}`,
+              `https://nnextbus.nus.edu.sg/ShuttleService?busstopname=${stopName}`,
               {
                 method: "GET",
                 headers: {
-                  AccountKey: process.env.LTA_DATAMALL_KEY,
+                  Authorization: `Basic ${encodedCredentials}`,
                 },
               }
             );
@@ -231,35 +137,139 @@ async function getArrivalTime(busStopsArray) {
             // Check if the response is ok and has a body
             if (!response.ok) {
               throw new Error(
-                `HTTP error from datamall! status: ${response.status}`
+                `HTTP error from NUSNextBus API! status: ${response.status}`
               );
             }
 
             const text = await response.text();
             if (!text) {
-              throw new Error("Empty response body from datamall");
+              throw new Error("Empty response body from NUSNextBus API");
             }
 
-            const datamallReply = JSON.parse(text);
-            if (!datamallReply.Services) {
-              throw new Error("Unexpected response format from datamall");
+            const NUSReply = JSON.parse(text);
+            // We will process the NUSReply in a 2 step process:
+            // 1) reformat reply such that we can search the buses by name in a dict.
+            // 2) iterate through buses in our bus stop objects and retrieve the timings based on the name.
+
+            // REFORMAT
+            let shuttles = {};
+            for (let shuttle of NUSReply.ShuttleServiceResult.shuttles) {
+              shuttles[shuttle.name] = shuttle;
             }
-            if (datamallReply.Services.length == 0) {
-              bus.timings = ["N.A.", "N.A."];
-            } else {
-              const firstArrivalTime =
-                datamallReply.Services[0].NextBus.EstimatedArrival;
-              const secondArrivalTime =
-                datamallReply.Services[0].NextBus2.EstimatedArrival;
-              bus.timings = [firstArrivalTime, secondArrivalTime];
+            // ITERATE AND UPDATE BUS ARRIVAL TIMING
+            for (let busObject of busStop.savedBuses) {
+              const serviceName = busObject.busNumber;
+              if (shuttles[serviceName]) {
+                const shuttle = shuttles[serviceName];
+                if (shuttle._etas) {
+                  // These are NUS buses. Public buses do not have ._etas field in NUSNextBus API response.
+                  // Handle the cases of differing sizes of etas returned due to 0/1/2 next buses.
+                  // ETA is not given in ISO time, so we have to calculate the ISO time based on mins till arrival.
+                  const etaLength = shuttle._etas.length;
+                  const currentTime = new Date();
+                  if (etaLength == 0) {
+                    busObject.timings = ["N.A.", "N.A."];
+                  } else if (etaLength == 1) {
+                    const arrivalTime = shuttle._etas[0].eta_s;
+                    const firstArrivalTime = new Date(
+                      currentTime.getTime() + arrivalTime * 1000
+                    ).toISOString();
+                    busObject.timings = [firstArrivalTime, "N.A."];
+                  } else {
+                    const arrivalTime = shuttle._etas[0].eta_s;
+                    const nextArrivalTime = shuttle._etas[1].eta_s;
+                    const firstArrivalTime = new Date(
+                      currentTime.getTime() + arrivalTime * 1000
+                    ).toISOString();
+                    const secondArrivalTime = new Date(
+                      currentTime.getTime() + nextArrivalTime * 1000
+                    ).toISOString();
+                    busObject.timings = [firstArrivalTime, secondArrivalTime];
+                    busStop.busNumber = shuttle.caption; // Update NUS Bus Stop name to be the full name rather than the code name (i.e. YIH-OPP -> Opp Yusof Ishak House).
+                  }
+                } else {
+                  // Public bus timings obtained by NUSNextBus API is given in mins to arrival rather than ISO time.
+                  // The code below converts arrival time in minutes to ISO time to maintain a standard response format.
+                  const currentTime = new Date();
+                  let arrivalTime = shuttles[serviceName].arrivalTime;
+                  let nextArrivalTime = shuttles[serviceName].nextArrivalTime;
+
+                  // Check if arrivalTime and nextArrivalTime are numbers (they can be Arr), if not, set them to 0
+                  arrivalTime = isNaN(arrivalTime) ? 0 : arrivalTime;
+                  nextArrivalTime = isNaN(nextArrivalTime)
+                    ? 0
+                    : nextArrivalTime;
+
+                  const firstArrivalTime = new Date(
+                    currentTime.getTime() + arrivalTime * 60000
+                  ).toISOString();
+                  const secondArrivalTime = new Date(
+                    currentTime.getTime() + nextArrivalTime * 60000
+                  ).toISOString();
+
+                  busObject.timings = [firstArrivalTime, secondArrivalTime];
+                }
+              }
             }
+            // Update NUS Bus Stop name to be the full name rather than the code name (i.e. NUSSTOP_YIH-OPP -> NUSSTOP_Opp Yusof Ishak House).
+            busStop.busStopName =
+              "NUSSTOP_" + NUSReply.ShuttleServiceResult.caption;
           } catch (error) {
-            console.error("Error fetching data from datamall:", error);
+            console.error("Error fetching data from NUSNextBus API:", error);
           }
-        })(busStop.busStopId, bus.busNumber);
+        })(busStop);
+      } else {
+        await Promise.all(
+          busStop.savedBuses.map(async (bus) => {
+            // logic is somewhat convoluted here, can be simplified by removing serviceNo to reduce the number of API calls.
+            // though that will require some gymnastics with inserting the timings (need to match serviceNo etc).
+            // Also, if we simply call the API by bus stop id, certain bus services will be missing because the api only responses for certain bus stops.
+            await (async (stopId, serviceNo) => {
+              try {
+                const response = await fetch(
+                  `http://datamall2.mytransport.sg/ltaodataservice/BusArrivalv2?BusStopCode=${stopId}&ServiceNo=${serviceNo}`,
+                  {
+                    method: "GET",
+                    headers: {
+                      AccountKey: process.env.LTA_DATAMALL_KEY,
+                    },
+                  }
+                );
+
+                // Check if the response is ok and has a body
+                if (!response.ok) {
+                  throw new Error(
+                    `HTTP error from datamall! status: ${response.status}`
+                  );
+                }
+
+                const text = await response.text();
+                if (!text) {
+                  throw new Error("Empty response body from datamall");
+                }
+
+                const datamallReply = JSON.parse(text);
+                if (!datamallReply.Services) {
+                  throw new Error("Unexpected response format from datamall");
+                }
+                if (datamallReply.Services.length == 0) {
+                  bus.timings = ["N.A.", "N.A."];
+                } else {
+                  const firstArrivalTime =
+                    datamallReply.Services[0].NextBus.EstimatedArrival;
+                  const secondArrivalTime =
+                    datamallReply.Services[0].NextBus2.EstimatedArrival;
+                  bus.timings = [firstArrivalTime, secondArrivalTime];
+                }
+              } catch (error) {
+                console.error("Error fetching data from datamall:", error);
+              }
+            })(busStop.busStopId, bus.busNumber);
+          })
+        );
       }
-    }
-  }
+    })
+  );
 }
 
 // GET request that takes location coordinates and returns a busStops object with updated arrival timings of buses.
