e25cc34a107f4cce4977fd68a0e730861fbe733b
leyew <102467346+itsme-zeix@users.noreply.github.com>
2024-06-30 20:57:21 +0900

    Feat [Local Storage]: Added frontend local async storage implementation and api request

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,11 +1,22 @@
-import { NavigationContainer, DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
-import Toast from 'react-native-toast-message';
-import { useColorScheme } from '@/hooks/useColorScheme';
+import {
+  NavigationContainer,
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+import "react-native-reanimated";
+import Toast from "react-native-toast-message";
+import { useColorScheme } from "@/hooks/useColorScheme";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from "@tanstack/react-query";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -26,16 +37,48 @@ export default function RootLayout() {
     return null;
   }
 
+  // One time setup for async storage
+  useEffect(() => {
+    const setupData = async () => {
+      const isInitialized = await AsyncStorage.getItem("isInitialized");
+      const lastUpdated = await AsyncStorage.getItem("lastUpdated");
+      if (!isInitialized) {
+        // Perform API call to retrieve database
+        const {
+          isPending,
+          error,
+          data: busStops,
+        } = useQuery({
+          queryKey: ["busStopDatabase"],
+          queryFn: () =>
+            fetch(
+              `http://localhost:3000/busStopsByLocation?latitude=${lastUpdated}`
+            ).then((res) => res.json()),
+        });
+
+        await AsyncStorage.setItem("busStops", JSON.stringify(busStops));
+        await AsyncStorage.setItem("isInitialized", "true");
+      }
+      if (isInitialized) {
+        // perform updating of data by pinging rest api.
+        // wait for implementation of last updated checks in backend?
+        // doesn't quite make sense to pull the entire database every time the app boots.
+      }
+    };
+
+    setupData();
+  }, []);
+
   // <Stack.Screen name="DetailedRoutingScreen" component={RouteCard}/>
   return (
     <>
-      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
         <Stack>
           <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
           <Stack.Screen name="+not-found" />
         </Stack>
       </ThemeProvider>
-      <Toast/>
+      <Toast />
     </>
-  );  
+  );
 }
