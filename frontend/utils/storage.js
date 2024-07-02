fcc73a167ff159b83868c0755dfc49c6bad7aa3d
leyew <102467346+itsme-zeix@users.noreply.github.com>
2024-07-02 18:42:47 +0800

    Feat [Local Storage]: Add utility file to store methods that take info from storage

---
+++ frontend/utils/storage.js
@@ -0,0 +1,18 @@
+// utils/storage.js
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+export const getFavouritedBusStops = async () => {
+  try {
+    const busStops = await AsyncStorage.getItem("busStops");
+    let busStopsArray = []
+    if (busStops !== null) {
+      busStopsArray = JSON.parse(busStops);
+
+      return busStopsArray.filter((busStop) => busStop.isFavourited);
+    }
+    return [];
+  } catch (error) {
+    console.error("Failed to fetch favourited bus stops from storage", error);
+    return [];
+  }
+};
