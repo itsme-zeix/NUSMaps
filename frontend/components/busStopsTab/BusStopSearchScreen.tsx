4ab4eabad47c23ed3b007c91732eeb87ee6a35f0
leyew <102467346+itsme-zeix@users.noreply.github.com>
2024-07-22 22:15:16 +0800

    Feat [Bus Stop Search Screen]: Added NUS Tag and name mapping for NUS stops

--- frontend/components/busStopsTab/BusStopSearchScreen.tsx
+++ frontend/components/busStopsTab/BusStopSearchScreen.tsx
@@ -1,24 +1,49 @@
-// src/app/busStopScreens/BusStopsSearchResultsScreen.tsx
 import React, { useEffect, useState } from "react";
-import {
-  SafeAreaView,
-  FlatList,
-  View,
-  Text,
-  StyleSheet,
-  TouchableOpacity,
-  ActivityIndicator,
-} from "react-native";
+import { SafeAreaView, FlatList, View, Text, StyleSheet, TouchableOpacity, ActivityIndicator } from "react-native";
 import { SearchBar, Icon } from "@rneui/base";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import {
-  useNavigation,
-  useRoute,
-  useFocusEffect,
-} from "@react-navigation/native";
+import { useNavigation, useRoute, useFocusEffect } from "@react-navigation/native";
 import { Ionicons } from "@expo/vector-icons";
+import { NUSTag } from "@/components/busStopsTab/NUSTag";
 
 const ITEMS_PER_PAGE = 20; // Number of items to load per page
+
+const mapNUSCodeNameToFullName = [
+  { value: "All", label: "All" },
+  { value: "COM3", label: "COM 3" },
+  { value: "TCOMS-OPP", label: "Opp TCOMS" },
+  { value: "PGP", label: "Prince George's Park" },
+  { value: "KR-MRT", label: "Kent Ridge MRT" },
+  { value: "LT27", label: "LT 27" },
+  { value: "UHALL", label: "University Hall" },
+  { value: "UHC-OPP", label: "Opp University Health Centre" },
+  { value: "MUSEUM", label: "Museum" },
+  { value: "UTOWN", label: "University Town" },
+  { value: "UHC", label: "University Health Centre" },
+  { value: "UHALL-OPP", label: "Opp University Hall" },
+  { value: "S17", label: "S17" },
+  { value: "KR-MRT-OPP", label: "Opp Kent Ridge MRT" },
+  { value: "PGPR", label: "Prince George's Park Foyer" },
+  { value: "TCOMS", label: "TCOMS" },
+  { value: "HSSML-OPP", label: "Opp HSSML" },
+  { value: "NUSS-OPP", label: "Opp NUSS" },
+  { value: "LT13-OPP", label: "Ventus" },
+  { value: "IT", label: "Information Technology" },
+  { value: "YIH-OPP", label: "Opp Yusof Ishak House" },
+  { value: "YIH", label: "Yusof Ishak House" },
+  { value: "CLB", label: "Central Library" },
+  { value: "LT13", label: "LT13" },
+  { value: "AS5", label: "AS5" },
+  { value: "BIZ2", label: "BIZ 2" },
+  { value: "KRB", label: "Kent Ridge Bus Terminal" },
+  { value: "SDE3-OPP", label: "Opp SDE 3" },
+  { value: "JP-SCH-16151", label: "The Japanese Primary School" },
+  { value: "KV", label: "Kent Vale" },
+  { value: "OTH", label: "Oei Tiong Ham Building" },
+  { value: "BG-MRT", label: "Botanic Gardens MRT (PUDO)" },
+  { value: "CG", label: "College Green" },
+  { value: "RAFFLES", label: "Raffles Hall" },
+];
 
 const BusStopSearchScreen = () => {
   const route = useRoute();
@@ -36,6 +61,7 @@ const BusStopSearchScreen = () => {
       try {
         const storedData = await AsyncStorage.getItem("busStops");
         let parsedData = storedData ? JSON.parse(storedData) : [];
+        parsedData = mapBusStopNames(parsedData);
         setData(parsedData);
         setFilteredData(parsedData.slice(0, ITEMS_PER_PAGE)); // Load the first page
       } catch (error) {
@@ -59,9 +85,7 @@ const BusStopSearchScreen = () => {
 
   const updateSearch = (search: string) => {
     setSearch(search);
-    const newData = data.filter((item) =>
-      item.busStopName.toLowerCase().includes(search.toLowerCase())
-    );
+    const newData = data.filter((item) => item.busStopName.toLowerCase().includes(search.toLowerCase()));
     setFilteredData(newData.slice(0, ITEMS_PER_PAGE)); // Reset to the first page of results
     setPage(1); // Reset the page to 1
   };
@@ -83,17 +107,11 @@ const BusStopSearchScreen = () => {
   };
 
   const toggleFavourite = async (busStopId) => {
-    const updatedData = data.map((item) =>
-      item.busStopId === busStopId
-        ? { ...item, isFavourited: !item.isFavourited }
-        : item
-    );
+    const updatedData = data.map((item) => (item.busStopId === busStopId ? { ...item, isFavourited: !item.isFavourited } : item));
 
     setData(updatedData);
 
-    const newFilteredData = updatedData.filter((item) =>
-      item.busStopName.toLowerCase().includes(search.toLowerCase())
-    );
+    const newFilteredData = updatedData.filter((item) => item.busStopName.toLowerCase().includes(search.toLowerCase()));
 
     setFilteredData(newFilteredData.slice(0, page * ITEMS_PER_PAGE));
 
@@ -101,48 +119,43 @@ const BusStopSearchScreen = () => {
     await AsyncStorage.setItem("busStops", JSON.stringify(updatedData));
   };
 
+  const mapBusStopNames = (busStops) => {
+    return busStops.map((stop) => {
+      let isNUSStop = false;
+      if (stop.busStopName.startsWith("NUSSTOP_")) {
+        isNUSStop = true;
+        const code = stop.busStopName.replace("NUSSTOP_", "");
+        const fullNameEntry = mapNUSCodeNameToFullName.find((entry) => entry.value === code);
+        return {
+          ...stop,
+          busStopName: fullNameEntry ? fullNameEntry.label : stop.busStopName,
+          isNUSStop,
+        };
+      }
+      return { ...stop, isNUSStop };
+    });
+  };
+
   const renderItem = ({ item }) => (
     <View style={styles.item}>
       <Text style={styles.busStopText}>{item.busStopName}</Text>
-      <TouchableOpacity onPress={() => toggleFavourite(item.busStopId)}>
-        <Icon
-          name={item.isFavourited ? "star" : "star-outline"}
-          type="ionicon"
-          color={item.isFavourited ? "#FFD700" : "#000"}
-        />
-      </TouchableOpacity>
+      <View style={styles.iconContainer}>
+        {item.isNUSStop && <NUSTag/>}
+        <TouchableOpacity onPress={() => toggleFavourite(item.busStopId)}>
+          <Icon name={item.isFavourited ? "star" : "star-outline"} type="ionicon" color={item.isFavourited ? "#FFD700" : "#000"} style={{ marginLeft: 5 }}  />
+        </TouchableOpacity>
+      </View>
     </View>
   );
 
   return (
     <SafeAreaView style={styles.container}>
       <View style={{ marginHorizontal: 10 }}>
-        <TouchableOpacity
-          onPress={() => navigation.goBack()}
-          style={styles.closeButton}
-        >
+        <TouchableOpacity onPress={() => navigation.goBack()} style={styles.closeButton}>
           <Text>Close</Text>
         </TouchableOpacity>
-        <SearchBar
-          placeholder="Search"
-          onChangeText={(text) => updateSearch(text)}
-          value={search}
-          platform="ios"
-          searchIcon={<Ionicons name="search" size={20} color="gray" />}
-          clearIcon={<Ionicons name="close-circle" size={23} color="gray" />}
-          autoFocus={true}
-          ref={(ref) => setSearchBarRef(ref)}
-        />
-        <FlatList
-          data={filteredData}
-          keyExtractor={(item) => item.busStopId}
-          renderItem={renderItem}
-          onEndReached={loadMoreData}
-          onEndReachedThreshold={0.5}
-          ListFooterComponent={
-            loading ? <ActivityIndicator size="large" /> : null
-          }
-        />
+        <SearchBar placeholder="Search" onChangeText={(text) => updateSearch(text)} value={search} platform="ios" searchIcon={<Ionicons name="search" size={20} color="gray" />} clearIcon={<Ionicons name="close-circle" size={23} color="gray" />} autoFocus={true} ref={(ref) => setSearchBarRef(ref)} />
+        <FlatList data={filteredData} keyExtractor={(item) => item.busStopId} renderItem={renderItem} onEndReached={loadMoreData} onEndReachedThreshold={0.5} ListFooterComponent={loading ? <ActivityIndicator size="large" /> : null} />
       </View>
     </SafeAreaView>
   );
@@ -163,6 +176,10 @@ const styles = StyleSheet.create({
     borderBottomColor: "#ccc",
     flexDirection: "row",
     justifyContent: "space-between",
+    alignItems: "center",
+  },
+  iconContainer: {
+    flexDirection: "row",
     alignItems: "center",
   },
   busStopText: {
