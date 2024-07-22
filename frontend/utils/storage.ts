38be5926df717de89122ef046ab4e9ac1de5c1aa
leyew <102467346+itsme-zeix@users.noreply.github.com>
2024-07-22 21:07:39 +0800

    convert storage.js to typescript

---
+++ frontend/utils/storage.ts
@@ -0,0 +1,17 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+export const getFavouritedBusStops = async () => {
+  try {
+    const busStops = await AsyncStorage.getItem("busStops");
+    let busStopsArray = [];
+    if (busStops !== null) {
+      busStopsArray = JSON.parse(busStops);
+
+      return busStopsArray.filter((busStop) => busStop.isFavourited);
+    }
+    return [];
+  } catch (error) {
+    console.error("Failed to fetch favourited bus stops from storage", error);
+    return [];
+  }
+};
