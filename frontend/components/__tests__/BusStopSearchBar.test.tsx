5158cd4cd9a844c1e34c1a9b76096f98412400a9
leyew <102467346+itsme-zeix@users.noreply.github.com>
2024-07-28 15:23:33 +0800

    fix linting

--- frontend/components/__tests__/BusStopSearchBar.test.tsx
+++ frontend/components/__tests__/BusStopSearchBar.test.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import { render, fireEvent } from '@testing-library/react-native';
-import BusStopSearchBar from "@/components/busStopsTab/BusStopSearchBar"// Adjust the path as necessary
-import { NavigationContainer } from '@react-navigation/native';
+import React from "react";
+import { render, fireEvent } from "@testing-library/react-native";
+import BusStopSearchBar from "@/components/busStopsTab/BusStopSearchBar"; // Adjust the path as necessary
+import { NavigationContainer } from "@react-navigation/native";
 
 // Mock the useNavigation hook
 const mockNavigate = jest.fn();
 
-jest.mock('@react-navigation/native', () => {
-  const actualNav = jest.requireActual('@react-navigation/native');
+jest.mock("@react-navigation/native", () => {
+  const actualNav = jest.requireActual("@react-navigation/native");
   return {
     ...actualNav,
     useNavigation: () => ({
@@ -16,27 +16,27 @@ jest.mock('@react-navigation/native', () => {
   };
 });
 
-describe('BusStopSearchBar', () => {
-  it('renders correctly', () => {
+describe("BusStopSearchBar", () => {
+  it("renders correctly", () => {
     const { getByText } = render(
       <NavigationContainer>
         <BusStopSearchBar />
       </NavigationContainer>
     );
 
-    expect(getByText('Search')).toBeTruthy();
+    expect(getByText("Search")).toBeTruthy();
   });
 
-  it('navigates to BusStopSearchScreen on press', () => {
+  it("navigates to BusStopSearchScreen on press", () => {
     const { getByText } = render(
       <NavigationContainer>
         <BusStopSearchBar />
       </NavigationContainer>
     );
 
-    const searchButton = getByText('Search');
+    const searchButton = getByText("Search");
     fireEvent.press(searchButton);
 
-    expect(mockNavigate).toHaveBeenCalledWith('BusStopSearchScreen', { initialQuery: '' });
+    expect(mockNavigate).toHaveBeenCalledWith("BusStopSearchScreen", { initialQuery: "" });
   });
 });
