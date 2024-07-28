3dc001d302e91532ca3533203dea05d0c923e801
leyew <102467346+itsme-zeix@users.noreply.github.com>
2024-07-28 19:29:09 +0800

    UI Style [Bus Stops Tab - Active Bus Marker]: Update colours and overall style

--- frontend/components/busServicesLive/activeBusMarker.tsx
+++ frontend/components/busServicesLive/activeBusMarker.tsx
@@ -1,18 +1,18 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { Marker, LatLng } from 'react-native-maps';
-import { FontAwesome5 } from '@expo/vector-icons';
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { Marker, LatLng } from "react-native-maps";
+import { FontAwesome5 } from "@expo/vector-icons";
 
 const determineBG = (crowdLevel: string) => {
-    if (crowdLevel == "low") {
-        return "green";
-    } else if (crowdLevel == "medium") {
-        return "yellow";
-    } else if (crowdLevel == "high") {
-        return "red";
-    } else {
-        return "grey";
-    }
+  if (crowdLevel == "low") {
+    return "green";
+  } else if (crowdLevel == "medium") {
+    return "yellow";
+  } else if (crowdLevel == "high") {
+    return "red";
+  } else {
+    return "grey";
+  }
 };
 
 type ActiveBusMarkerProps = {
@@ -24,43 +24,69 @@ type ActiveBusMarkerProps = {
 const ActiveBusMarker: React.FC<ActiveBusMarkerProps> = ({ coordinate, vehicleLicensePlate, crowdLevel }) => {
   return (
     <Marker coordinate={coordinate}>
-      <View style={styles.stopContainer}>
-        <View style={[{ backgroundColor: determineBG(crowdLevel) }, styles.crowdLevelRectangle]} />
-        <View style={styles.textContainer}>
-          <Text style={styles.markerText}>{vehicleLicensePlate}</Text>
+      <View style={styles.container}>
+        <View style={styles.infoContainer}>
+          <View style={[{ backgroundColor: determineBG(crowdLevel) }, styles.crowdLevelRectangle]} />
+          <View style={styles.textContainer}>
+            <Text style={styles.markerText}>{vehicleLicensePlate}</Text>
+          </View>
+        </View>
+        <View style={styles.busIconContainer}>
+          <FontAwesome5 name="bus" size={16} color="white" style={styles.iconStyle} />
         </View>
       </View>
-      <FontAwesome5 name="bus" size={16} color="orange" style={styles.iconStyle} />
     </Marker>
   );
 };
 
 const styles = StyleSheet.create({
-  stopContainer: {
-    flexDirection: 'column', // Layout children in a row
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: 'orange', // Set the background color to orange
-    padding: 5,
-    borderRadius: 5,
+  container: {
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    shadowColor: "#828282",
+    shadowOpacity: 0.3,
+    shadowOffset: { width: 1, height: 4 },
+  },
+  infoContainer: {
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "white",
+    paddingVertical: 3,
+    paddingHorizontal: 3,
+    borderRadius: 7,
+    borderWidth: 2,
+    borderColor: "#EF7C01",
   },
   crowdLevelRectangle: {
-    width:20,
-    height:5
+    width: 35,
+    height: 5,
+    margin: 2,
   },
   textContainer: {
-    flexDirection:'row'
+    flexDirection: "row",
   },
   markerText: {
-    color: 'white',
+    color: "black",
     padding: 2,
     borderRadius: 3,
-    textAlign: 'center',
-    fontWeight: 'bold',
-    fontSize:8
+    textAlign: "center",
+    fontWeight: "bold",
+    fontSize: 8,
+  },
+  busIconContainer: {
+    backgroundColor: "#EF7C01",
+    height: 20,
+    width: 20,
+    borderRadius: 3,
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 3,
   },
   iconStyle: {
-    alignSelf:'center'
+    alignSelf: "center",
   },
 });
 
