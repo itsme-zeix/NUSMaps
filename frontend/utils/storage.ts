0ae4ac29373706fef73fcaee9fc6f1130cb4d90f
leyew <102467346+itsme-zeix@users.noreply.github.com>
2024-07-30 00:43:30 +0800

    Refactor [AsyncStorage Util]: Add types for TypeScript

--- frontend/utils/storage.ts
+++ frontend/utils/storage.ts
@@ -1,5 +1,19 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
+interface BusService {
+  busNumber: string;
+  timings: string[]; // ISO format
+}
+interface BusStop {
+  busStopName: string;
+  busStopId: string;
+  distanceAway: string;
+  savedBuses: BusService[];
+  latitude: number;
+  longitude: number;
+  isFavourited: boolean;
+}
+
 export const getFavouritedBusStops = async () => {
   try {
     const busStops = await AsyncStorage.getItem("busStops");
@@ -7,7 +21,7 @@ export const getFavouritedBusStops = async () => {
     if (busStops !== null) {
       busStopsArray = JSON.parse(busStops);
 
-      return busStopsArray.filter((busStop) => busStop.isFavourited);
+      return busStopsArray.filter((busStop: BusStop) => busStop.isFavourited);
     }
     return [];
   } catch (error) {
