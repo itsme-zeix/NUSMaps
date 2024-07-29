974bab26230be71554aca475e700a80db27be5fa
Zheng Shun Ren <zhengshunren@outlook.com>
2024-07-29 13:55:23 +0800

    fix: changed babel.config.cjs

--- frontend/babel.config.cjs
+++ frontend/babel.config.cjs
@@ -1,8 +1,9 @@
 module.exports = function (api) {
-  api.cache(true);
-  const isTest = api.env('test');
+  api.cache(() => process.env.NODE_ENV === 'production');
   const presets = ["babel-preset-expo", '@babel/preset-typescript'];
   const plugins = [];
+  // Check the environment and conditionally add plugins
+  const isTest = process.env.NODE_ENV === 'test';
   if (isTest) {
     plugins.push(
       ['@babel/plugin-transform-class-properties', { loose: true }],
