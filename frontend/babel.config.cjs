58379479086fb03b353e8b67305188637f7f416e
Zheng Shun Ren <zhengshunren@outlook.com>
2024-07-29 13:41:01 +0800

    feat: modified babel.config.cjs file to detect if unit tests, to ensure mapview doesnt break + github actions workflow for running jest tests

--- frontend/babel.config.cjs
+++ frontend/babel.config.cjs
@@ -1,12 +1,18 @@
 module.exports = function (api) {
   api.cache(true);
-  return {
-    presets: ["babel-preset-expo", '@babel/preset-typescript'],
-    plugins: [
+  const isTest = api.env('test');
+  const presets = ["babel-preset-expo", '@babel/preset-typescript'];
+  const plugins = [];
+  if (isTest) {
+    plugins.push(
       ['@babel/plugin-transform-class-properties', { loose: true }],
       ['@babel/plugin-transform-private-methods', { loose: true }],
       ['@babel/plugin-transform-private-property-in-object', { loose: true }],
       ["react-native-reanimated/plugin"]
-  ],
+    );
+  }
+  return {
+    presets,
+    plugins, 
   };
 };
