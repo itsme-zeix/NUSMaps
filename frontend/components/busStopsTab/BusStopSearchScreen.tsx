265ec87541a73ddf32a3886200c2d9e7a0ecf469
leyew <102467346+itsme-zeix@users.noreply.github.com>
2024-08-01 12:37:30 +0800

    Refactor [BusStopSearchScreen]: Migrate to util for map NUS code name to full name

--- frontend/components/busStopsTab/BusStopSearchScreen.tsx
+++ frontend/components/busStopsTab/BusStopSearchScreen.tsx
@@ -8,6 +8,7 @@ import { NUSTag } from "@/components/busStopsTab/NUSTag";
 import { RouteProp } from "@react-navigation/native";
 import { StackNavigationProp } from "@react-navigation/stack";
 import Toast from "react-native-toast-message";
+import { mapNUSCodeNametoFullName } from "@/utils/mapNUSCodeNametoFullName";
 
 type RootStackParamList = {
   BusStopSearch: { initialQuery: string };
@@ -20,43 +21,6 @@ interface BusStopItem {
   busStopName: string;
   isFavourited: boolean;
 }
-
-const mapNUSCodeNameToFullName = [
-  { value: "All", label: "All" },
-  { value: "COM3", label: "COM 3" },
-  { value: "TCOMS-OPP", label: "Opp TCOMS" },
-  { value: "PGP", label: "Prince George's Park" },
-  { value: "KR-MRT", label: "Kent Ridge MRT" },
-  { value: "LT27", label: "LT 27" },
-  { value: "UHALL", label: "University Hall" },
-  { value: "UHC-OPP", label: "Opp University Health Centre" },
-  { value: "MUSEUM", label: "Museum" },
-  { value: "UTOWN", label: "University Town" },
-  { value: "UHC", label: "University Health Centre" },
-  { value: "UHALL-OPP", label: "Opp University Hall" },
-  { value: "S17", label: "S17" },
-  { value: "KR-MRT-OPP", label: "Opp Kent Ridge MRT" },
-  { value: "PGPR", label: "Prince George's Park Foyer" },
-  { value: "TCOMS", label: "TCOMS" },
-  { value: "HSSML-OPP", label: "Opp HSSML" },
-  { value: "NUSS-OPP", label: "Opp NUSS" },
-  { value: "LT13-OPP", label: "Ventus" },
-  { value: "IT", label: "Information Technology" },
-  { value: "YIH-OPP", label: "Opp Yusof Ishak House" },
-  { value: "YIH", label: "Yusof Ishak House" },
-  { value: "CLB", label: "Central Library" },
-  { value: "LT13", label: "LT13" },
-  { value: "AS5", label: "AS5" },
-  { value: "BIZ2", label: "BIZ 2" },
-  { value: "KRB", label: "Kent Ridge Bus Terminal" },
-  { value: "SDE3-OPP", label: "Opp SDE 3" },
-  { value: "JP-SCH-16151", label: "The Japanese Primary School" },
-  { value: "KV", label: "Kent Vale" },
-  { value: "OTH", label: "Oei Tiong Ham Building" },
-  { value: "BG-MRT", label: "Botanic Gardens MRT (PUDO)" },
-  { value: "CG", label: "College Green" },
-  { value: "RAFFLES", label: "Raffles Hall" },
-];
 
 const BusStopSearchScreen: React.FC = () => {
   const route = useRoute<BusStopSearchRouteProp>();
@@ -97,20 +61,11 @@ const BusStopSearchScreen: React.FC = () => {
     }, [searchBarRef])
   );
 
-  const mapBusStopNames = (busStopName: string) => {
-    if (busStopName.startsWith("NUSSTOP_")) {
-      const code = busStopName.replace("NUSSTOP_", "");
-      const fullNameEntry = mapNUSCodeNameToFullName.find((entry) => entry.value === code);
-      return fullNameEntry ? fullNameEntry.label : busStopName;
-    }
-    return busStopName;
-  };
-
   const updateSearch = (search: string) => {
     setSearch(search);
     const newData = data.filter((item) => {
       const busStopName = item.busStopName ? item.busStopName.toLowerCase() : "";
-      const fullBusStopName = busStopName.startsWith("nusstop_") ? mapBusStopNames(item.busStopName).toLowerCase() : "";
+      const fullBusStopName = busStopName.startsWith("nusstop_") ? mapNUSCodeNametoFullName(item.busStopName).toLowerCase() : "";
       const busStopId = item.busStopId ? item.busStopId.toString() : "";
       return busStopName.includes(search.toLowerCase()) || busStopId.includes(search.toLowerCase()) || fullBusStopName.includes(search.toLowerCase());
     });
@@ -124,7 +79,7 @@ const BusStopSearchScreen: React.FC = () => {
 
     const newFilteredData = updatedData.filter((item) => {
       const busStopName = item.busStopName ? item.busStopName.toLowerCase() : "";
-      const fullBusStopName = busStopName.startsWith("nusstop_") ? mapBusStopNames(item.busStopName).toLowerCase() : "";
+      const fullBusStopName = busStopName.startsWith("nusstop_") ? mapNUSCodeNametoFullName(item.busStopName).toLowerCase() : "";
       const busStopId = item.busStopId ? item.busStopId.toString() : "";
       return busStopName.includes(search.toLowerCase()) || busStopId.includes(search.toLowerCase()) || fullBusStopName.includes(search.toLowerCase());
     });
@@ -154,7 +109,7 @@ const BusStopSearchScreen: React.FC = () => {
 
   const renderItem = ({ item }: { item: BusStopItem }) => {
     const isNUSStop = item.busStopName.startsWith("NUSSTOP_");
-    const busStopName = mapBusStopNames(item.busStopName);
+    const busStopName = mapNUSCodeNametoFullName(item.busStopName);
 
     return (
       <View style={styles.item}>
