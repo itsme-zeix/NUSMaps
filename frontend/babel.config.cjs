fdededac3cece45c48f7acabb6935553b79e5429
leyew <102467346+itsme-zeix@users.noreply.github.com>
2024-08-03 21:48:45 +0800

    Add old babel config for jest testing

--- frontend/babel.config.cjs
+++ frontend/babel.config.cjs
@@ -1,36 +1,36 @@
-// module.exports = function (api) {
-//   console.log(process.env.NODE_ENV)
-//   api.cache(() => process.env.NODE_ENV === "production");
-//   const presets = ["babel-preset-expo", "@babel/preset-typescript"];
-//   const plugins = [];
-//   // Check the environment and conditionally add plugins
-//   const isTest = process.env.NODE_ENV === "test";
-//   if (isTest) {
-//     plugins.push(
-//       ["@babel/plugin-transform-class-properties", { loose: true }],
-//       ["@babel/plugin-transform-private-methods", { loose: true }],
-//       ["@babel/plugin-transform-private-property-in-object", { loose: true }],
-//       ["react-native-reanimated/plugin"]
-//     );
-//   }
-//   return {
-//     presets,
-//     plugins,
-//   };
-// }
-
 module.exports = function (api) {
-  api.cache(true);
+  console.log(process.env.NODE_ENV)
+  api.cache(() => process.env.NODE_ENV === "production");
+  const presets = ["babel-preset-expo", "@babel/preset-typescript"];
+  const plugins = [];
+  // Check the environment and conditionally add plugins
+  const isTest = process.env.NODE_ENV === "test";
+  if (isTest) {
+    plugins.push(
+      ["@babel/plugin-transform-class-properties", { loose: true }],
+      ["@babel/plugin-transform-private-methods", { loose: true }],
+      ["@babel/plugin-transform-private-property-in-object", { loose: true }],
+      ["react-native-reanimated/plugin"]
+    );
+  }
   return {
-    presets: ["babel-preset-expo"],
-    plugins: [
-      "react-native-reanimated/plugin",
-      [
-        "module-resolver",
-        {
-          root: ["./"],
-        },
-      ],
-    ],
+    presets,
+    plugins,
   };
-};
+}
+
+// module.exports = function (api) {
+//   api.cache(true);
+//   return {
+//     presets: ["babel-preset-expo"],
+//     plugins: [
+//       "react-native-reanimated/plugin",
+//       [
+//         "module-resolver",
+//         {
+//           root: ["./"],
+//         },
+//       ],
+//     ],
+//   };
+// };
