90d451d8e9d45738eadcfc32dbb87fd4de55856c
leyew <102467346+itsme-zeix@users.noreply.github.com>
2024-08-11 00:45:00 +0800

    Refactor [Bus Number]: Refactor colour assignment logic to be clearer

--- frontend/components/detailedRouteScreen/BusNumber.tsx
+++ frontend/components/detailedRouteScreen/BusNumber.tsx
@@ -3,22 +3,41 @@ import { View, StyleSheet, Text } from "react-native";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import { determineFontColor } from "./determineFontColor";
 
-interface busNumberType {
+interface BusNumberType {
   busNumber: string;
   busType: string;
-  testID:string;
+  testID: string;
 }
 
-export const BusNumberCard: React.FC<busNumberType> = ({ busNumber, busType, testID }) => {
-  const busBackgroundColor = busType === "NUS_BUS" ? busBGMapping[busNumber] : styles.PUBLICBUSBG;
-  const backgroundColor = busBackgroundColor.backgroundColor;
+const determineBusBG = (busNumber: string) => {
+  switch (busNumber) {
+    case "A1":
+      return "#FF0000";
+    case "A2":
+      return "#E4CE0C";
+    case "D1":
+      return "#CD82E2";
+    case "D2":
+      return "#6F1B6F";
+    case "BTC":
+      return "#EE8136";
+    case "K":
+      return "#345A9B";
+    case "L":
+      return "#BFBFBF";
+    default:
+      return "#4ABF50"; // Default to PUBLICBUSBG color
+  }
+};
+
+export const BusNumberCard: React.FC<BusNumberType> = ({ busNumber, busType, testID }) => {
+  const backgroundColor = busType === "NUS_BUS" ? determineBusBG(busNumber) : "#4ABF50";
   const fontColor = determineFontColor(backgroundColor);
 
-  
   return (
-    <View style={{ flexDirection: "row", alignItems: "center", justifyContent: "center"  }} testID={testID}>
+    <View style={{ flexDirection: "row", alignItems: "center", justifyContent: "center" }} testID={testID}>
       <Ionicons name="bus" size={23} color="#434343" style={{ marginRight: 3 }} />
-      <View style={[styles.busNumberContainer, busBackgroundColor]}>
+      <View style={[styles.busNumberContainer, { backgroundColor }]}>
         <Text style={[styles.busNumber, { color: fontColor }]}>{busNumber}</Text>
       </View>
     </View>
@@ -39,38 +58,4 @@ const styles = StyleSheet.create({
     fontFamily: "Inter-SemiBold",
     fontSize: 15,
   },
-  PUBLICBUSBG: {
-    backgroundColor: "#4ABF50",
-  },
-  D1: {
-    backgroundColor: "#CD82E2",
-  },
-  D2: {
-    backgroundColor: "#6F1B6F",
-  },
-  A1: {
-    backgroundColor: "#FF0000",
-  },
-  A2: {
-    backgroundColor: "#E4CE0C",
-  },
-  BTC: {
-    backgroundColor: "#EE8136",
-  },
-  K: {
-    backgroundColor: "#345A9B",
-  },
-  L: {
-    backgroundColor: "#BFBFBF",
-  },
 });
-
-const busBGMapping = {
-  A1: styles.A1,
-  A2: styles.A2,
-  D1: styles.D1,
-  D2: styles.D2,
-  BTC: styles.BTC,
-  K: styles.K,
-  L: styles.L,
-};
